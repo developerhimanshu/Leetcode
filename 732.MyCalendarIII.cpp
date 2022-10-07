d8b970bd28d47ce4c4cf6999644da39201b91eb6
Himanshu Singh <chamyalhemsingh@gmail.com>
2022-10-07 22:18:02 +0530

    732. My Calendar III

---
+++ 732.MyCalendarIII.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/my-calendar-iii/
+
+
+class MyCalendarThree {
+public:
+     map<int,int> mp;
+    MyCalendarThree() {
+        mp.clear();
+    }
+    
+    int book(int start, int end) {
+        mp[start]++;
+        mp[end]--;
+        int res=0, cur=0;
+        for(auto &e : mp){
+            cur += e.second;
+            res = max(res, cur);
+        }
+        return res;
+    }
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree* obj = new MyCalendarThree();
+ * int param_1 = obj->book(start,end);
+ */
